8134c12154c91509ccfa7c6abd0f15ffac58ccbf
Jules Larke <131803263+JulesLarke-USDA@users.noreply.github.com>
2024-07-16 12:07:25 -0700

    Update pipeline_foodcodes.sh

--- src/pipeline_foodcodes.sh
+++ src/pipeline_foodcodes.sh
@@ -8,6 +8,6 @@ echo "Initializing step 3: text matching discontinued codes"; date
 python3 ../lib/03_text_match_discontinued_codes.py
 echo "Initializing step 3b: Further ingredientizing with FCID"; date
 python3 ../lib/03b_fcid_ingredients.py
-echo "Initializing step 5: "Combining WWEIA foodcode data"; date
+echo "Initializing step 5: Combining WWEIA foodcode data"; date
 python3 ../lib/05_wweia_foodcodes.py
 echo "Success! Pipeline finished on:"; date
