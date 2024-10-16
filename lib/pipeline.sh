e25da53bcc9e1bfa29295181668b8ff604dafd53
Jules Larke <131803263+JulesLarke-USDA@users.noreply.github.com>
2024-10-16 15:21:21 -0700

    Update and rename pipeline_food_and_ingred_codes.sh to pipeline.sh
    updated script to new pipeline incorporating moisture change and retaining foodcodes in final output

--- lib/pipeline.sh
+++ lib/pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Initializing step 1: consolidating ingredient codes"; date
+echo "Initializing step 1: generating ingredient nutrient database"; date
 python3 ../src/01_generate_ingredient_nutrient_db.py
 echo "Initializing step 2: calculating ingredient weights"; date
 python3 ../src/02_calculate_ingredient_weights.py
@@ -8,6 +8,8 @@ echo "Initializing step 3: text matching discontinued codes"; date
 python3 ../src/03_text_match_discontinued_codes.py
 echo "Initializing step 3b: Further ingredientizing with FCID"; date
 python3 ../src/03b_fcid_ingredients.py
-echo "Initializing step 6: Combining WWEIA foodcode and ingredient code data"; date
-python3 ../src/06_wweia_food_matched_ingred_codes.py
+echo "Initializing step 3c: Adjusting moisture content"; date
+python3 ../src/03c_moisture_adjustment.py
+echo "Initializing step 4: Generating ingredientized diet recalls"; date
+python3 ../src/04_wweia_ingredients_2_day_foodcode.py
 echo "Success! Pipeline finished on:"; date
